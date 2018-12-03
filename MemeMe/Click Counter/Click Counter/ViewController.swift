f09bea4c8f7ca70fbcb08565512d203250c6fb01
Majd <kho.majds@gmail.com>
2018-12-02 22:09:14 -0500

    replacing outlet connections with storyboard rather than code

--- MemeMe/Click Counter/Click Counter/ViewController.swift
+++ MemeMe/Click Counter/Click Counter/ViewController.swift
@@ -10,13 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var count = 0 {
-        didSet {
-            view.backgroundColor = self.colors.randomElement()
-        }
-    }
-    var label: UILabel!
-    var label2: UILabel!
+    var count = 0
+//    {
+//        didSet {
+//            view.backgroundColor = self.colors.randomElement()
+//        }
+//    }
+    @IBOutlet var label: UILabel!
+//  var label2: UILabel!
     
     // color array
     let colors: [UIColor] = [.red, .blue, .yellow, .green, .black, .white]
@@ -26,44 +27,49 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         // label
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 150, width: 60, height:60)
-        label.text = "0"
-        view.addSubview(label)
-        self.label = label
-        
-        // label2
-        let label2 = UILabel()
-        label2.frame = CGRect(x: 210, y: 150, width: 60, height:60)
-        label2.text = "\(self.count)"
-        view.addSubview(label2)
-        self.label2 = label2
-        
-        //button
-        let button = UIButton()
-        button.frame = CGRect(x: 100, y: 250, width: 100, height: 60)
-        button.setTitle("Increment", for: .normal)
-        button.setTitleColor( .blue, for: .normal)
-        button.tag = 1
-        view.addSubview(button)
-        
-        button.addTarget(self, action: #selector(changeCount), for: UIControl.Event.touchUpInside)
-        
-        //button
-        let button2 = UIButton()
-        button2.frame = CGRect(x: 210, y: 250, width: 100, height: 60)
-        button2.setTitle("Decrement", for: .normal)
-        button2.setTitleColor( .red, for: .normal)
-        button2.tag = -1
-        view.addSubview(button2)
-        
-        button2.addTarget(self, action: #selector(changeCount), for: UIControl.Event.touchUpInside)
+//        let label = UILabel()
+//        label.frame = CGRect(x: 150, y: 150, width: 60, height:60)
+//        label.text = "0"
+//        view.addSubview(label)
+//        self.label = label
+//
+//        // label2
+//        let label2 = UILabel()
+//        label2.frame = CGRect(x: 210, y: 150, width: 60, height:60)
+//        label2.text = "\(self.count)"
+//        view.addSubview(label2)
+//        self.label2 = label2
+//
+//        //button
+//        let button = UIButton()
+//        button.frame = CGRect(x: 100, y: 250, width: 100, height: 60)
+//        button.setTitle("Increment", for: .normal)
+//        button.setTitleColor( .blue, for: .normal)
+//        button.tag = 1
+//        view.addSubview(button)
+//
+//        button.addTarget(self, action: #selector(changeCount), for: UIControl.Event.touchUpInside)
+//
+//        //button
+//        let button2 = UIButton()
+//        button2.frame = CGRect(x: 210, y: 250, width: 100, height: 60)
+//        button2.setTitle("Decrement", for: .normal)
+//        button2.setTitleColor( .red, for: .normal)
+//        button2.tag = -1
+//        view.addSubview(button2)
+//
+//        button2.addTarget(self, action: #selector(changeCount), for: UIControl.Event.touchUpInside)
     }
     
-    @objc func changeCount(sender: UIButton) {
-        self.count += sender.tag
+//    @objc func changeCount(sender: UIButton) {
+//        self.count += sender.tag
+//        self.label.text = "\(self.count)"
+//        self.label2.text = "\(self.count)"
+//    }
+    
+    @objc func incremenetCount() {
+        self.count += 1
         self.label.text = "\(self.count)"
-        self.label2.text = "\(self.count)"
     }
 }
 
