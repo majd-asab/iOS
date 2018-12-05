4904457e7eed7163941a9731fc9db629325691f5
Majd <kho.majds@gmail.com>
2018-12-04 20:57:46 -0500

    fixed error and changed color switch to slider

--- MemeMe/ios-nd-uikit-swift-3/step1.4-colorError3/ColorMaker/ViewController.swift
+++ MemeMe/ios-nd-uikit-swift-3/step1.4-colorError3/ColorMaker/ViewController.swift
@@ -15,26 +15,34 @@ class ViewController: UIViewController {
     // MARK: Outlets
     
     @IBOutlet weak var colorView: UIView!
-    @IBOutlet weak var redControl: UISwitch!
-    @IBOutlet weak var greenControl: UISwitch!
-    @IBOutlet weak var blueControl: UISwitch!
+    @IBOutlet weak var redControl: UISlider!
+    @IBOutlet weak var greenControl: UISlider!
+    @IBOutlet weak var blueControl: UISlider!
+    
 
     // MARK: Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.redControl.minimumTrackTintColor = UIColor.red
+        self.greenControl.minimumTrackTintColor = UIColor.green
+        self.blueControl.minimumTrackTintColor = UIColor.blue
+        
+        
     }
     
     // MARK: Actions
     
-    @IBAction func changeColorComponent(_ sender: AnyObject) {
+    @IBAction func changeColorComponent(_ sender: UISlider) {
         
-        let r: CGFloat = self.redControl.isOn ? 1 : 0
-        let g: CGFloat = self.greenControl.isOn ? 1 : 0
-        let b: CGFloat = self.blueControl.isOn ? 1 : 0
-                
-        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+        let r: CGFloat = CGFloat(self.redControl.value)
+        let g: CGFloat = CGFloat(self.greenControl.value)
+        let b: CGFloat = CGFloat(self.blueControl.value)
+        
+            colorView.backgroundColor = UIColor(
+                red: r, green: g, blue: b, alpha: 1)
+    
     }
 }
 
