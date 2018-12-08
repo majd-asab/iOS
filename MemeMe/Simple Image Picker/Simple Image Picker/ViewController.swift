83b0e6bd25661f52c82bbafc5a607e8907613e01
Majd <kho.majds@gmail.com>
2018-12-08 11:41:20 -0500

    added activity + alert modal views

--- MemeMe/Simple Image Picker/Simple Image Picker/ViewController.swift
+++ MemeMe/Simple Image Picker/Simple Image Picker/ViewController.swift
@@ -14,20 +14,60 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Image Picker
         let button =  UIButton()
-        button.frame = CGRect(x: 100, y: 300, width: 300, height: 60)
+        button.frame = CGRect(x: 60, y: 300, width: 300, height: 60)
         button.setTitle("display image picker", for: UIControl.State.normal)
         button.setTitleColor(UIColor.blue, for: UIControl.State.normal)
         self.view.addSubview(button)
         
-        button.addTarget(self, action: #selector(experiment), for: UIControl.Event.touchUpInside)
+        button.addTarget(self, action: #selector(imagePicker), for: UIControl.Event.touchUpInside)
+        
+        // Activity View
+        let button2 =  UIButton()
+        button2.frame = CGRect(x: 60, y: 400, width: 300, height: 60)
+        button2.setTitle("display activity view", for: UIControl.State.normal)
+        button2.setTitleColor(UIColor.blue, for: UIControl.State.normal)
+        self.view.addSubview(button2)
+        
+        button2.addTarget(self, action: #selector(self.activityView), for: UIControl.Event.touchUpInside)
+        
+        
+        // Alert View
+        let button3 =  UIButton()
+        button3.frame = CGRect(x: 60, y: 500, width: 300, height: 60)
+        button3.setTitle("display alert view", for: UIControl.State.normal)
+        button3.setTitleColor(UIColor.blue, for: UIControl.State.normal)
+        self.view.addSubview(button3)
+        
+        button3.addTarget(self, action: #selector(self.alertView), for: UIControl.Event.touchUpInside)
     }
     
-    @IBAction func experiment(sender: UIButton){
+    @IBAction func imagePicker(sender: UIButton){
         print(sender.currentTitle ?? "value not found")
         let nextController = UIImagePickerController()
         
         self.present(nextController, animated: true, completion: nil)
+    }
+    
+    @IBAction func activityView(sender: UIButton){
+        let image = UIImage()
+        let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        self.present(controller, animated: true, completion: nil)
+    }
+    
+    @IBAction func alertView(sender: UIButton){
+        let controller = UIAlertController()
+        controller.title = "Alert Test"
+        controller.message  = "alert test"
+        
+        //must add dismiss action manually
+        let okAction = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: {action in
+            self.dismiss(animated: true, completion: nil)
+        })
+        controller.addAction(okAction)
+        self.present(controller, animated: true, completion: nil)
     }
 
 
