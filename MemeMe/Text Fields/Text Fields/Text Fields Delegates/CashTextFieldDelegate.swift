e15d2be25c118515ec4ce3ba1b3f73aff248b634
Majd <kho.majds@gmail.com>
2018-12-21 17:49:50 -0500

    added Cash text field and UISwitch controlled textfield

---
+++ MemeMe/Text Fields/Text Fields/Text Fields Delegates/CashTextFieldDelegate.swift
@@ -0,0 +1,44 @@
+//
+//  CashTextFieldDelegate.swift
+//  Text Fields
+//
+//  Created by Majd on 2018-12-20.
+//  Copyright © 2018 HappyWorld. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK: A dollar sign field that begins with the text $0.00, and then fills in the dollar figure as digits are added. For example, typing 4-2-7-5 would produce $0.00, $0.04, $0.42, $4.27, $42.75.
+class CashTextFieldDelegate: NSObject, UITextFieldDelegate {
+    
+    let nums: Set<Character> = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    var cashArray: [String] = ["0", "0", "0", "0"]
+    var cashArrayPointer: Int = 4
+    
+    func returnArrayPointer() -> Int {
+        if cashArrayPointer - 1 <= -1 {
+            cashArrayPointer = 3
+        }else {
+            cashArrayPointer -= 1
+        }
+        return cashArrayPointer
+    }
+    
+    // Check if character is numberic
+    func isNumeric(string: String) -> Bool {
+        if string == "" || string.count > 1 { return false }
+        return Set(string).isSubset(of: self.nums)
+    }
+    
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        if self.isNumeric(string: string) {
+            cashArray[returnArrayPointer()] = string
+            let newString = "$\(cashArray[0])\(cashArray[1]).\(cashArray[2])\(cashArray[3])"
+            textField.text  = newString
+        }
+        return false
+    }
+}
--- MemeMe/Text Fields/Text Fields/ViewController.swift
+++ MemeMe/Text Fields/Text Fields/ViewController.swift
@@ -12,17 +12,39 @@ class ViewController: UIViewController, UITextFieldDelegate {
 
     // MARK: Outlets
     @IBOutlet weak var textField1: UITextField!
-    //    @IBOutlet weak var textField2: UITextField!
-//    @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var editSwitch: UISwitch!
     
     // MARK: Text Field Delegate objects
     let zipCodeTextFieldDelegate = ZipCodeTextFieldDelegate()
+    let cashTextFieldDelegate    = CashTextFieldDelegate()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         self.textField1.delegate = self.zipCodeTextFieldDelegate
+        self.textField2.delegate = self.cashTextFieldDelegate
+        self.textField3.delegate = self
+        
+        self.editSwitch.setOn(false, animated: false)
+    }
+    
+    @IBAction func onSwitchTouch(_ sender: AnyObject) {
+        if !(sender as! UISwitch).isOn {
+            self.textField3.resignFirstResponder()
+        }
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        return self.editSwitch.isOn == true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true;
     }
 
 
