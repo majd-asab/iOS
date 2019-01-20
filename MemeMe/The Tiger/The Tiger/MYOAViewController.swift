2a147969f0e0608515ae7649892be01174df4de3
Majd <kho.majds@gmail.com>
2019-01-19 23:05:37 -0500

    created my navigation controller based app

---
+++ MemeMe/The Tiger/The Tiger/MYOAViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MYOAViewController.swift
+//  The Tiger
+//
+//  Created by Majd on 2019-01-19.
+//  Copyright © 2019 HappyWorld. All rights reserved.
+//
+
+import UIKit
+
+class MYOAViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Start Over", style: .plain, target: self, action: #selector(startOver))
+    }
+    
+    @objc func startOver() {
+        if let navController = self.navigationController {
+            navController.popToRootViewController(animated: true)
+        }
+    }
+
+}
