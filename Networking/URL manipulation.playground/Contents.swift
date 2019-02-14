d6a3ed38d4f5d1d00b9d1c8c1fed04fa53b6633e
Majd <kho.majds@gmail.com>
2019-02-13 22:25:40 -0500

    started networking course

---
+++ Networking/URL manipulation.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+let website = "http://udacity.com"
+// -------------------------------
+// Task 1: Create a valid URL
+//         by calling URL(string:)
+// -------------------------------
+var url = URL(string: website)// failable initializer, its optional if it fails it returns nil
+
+// -------------------------------
+// Task 2: Append an "ios" path
+//         component to the URL
+//         with appendPathComponent(_:)
+// -------------------------------
+if var validUrl = url {
+    validUrl.appendPathComponent("ios")
+    print(validUrl)
+}
+// we suggest you print out your URL to verify the result
+
